f8f87d1e170e2995e9555e18a8fd78f39c444147
Daniel Gaiottino <daniel.gaiottino@gmail.com>
2016-10-26 21:48:26 +0200

    Info instead of debug

--- lib/restforce/concerns/verbs.rb
+++ lib/restforce/concerns/verbs.rb
@@ -35,7 +35,7 @@ module Restforce
             started_at = Time.now.to_i
             response = connection.send(verb, *args, &block)
             ended_at = Time.now.to_i
-            Rails.logger.debug "$$$ Restforce (#{ended_at - started_at}s) #{verb} #{args[0]}" if defined?(Rails)
+            Rails.logger.info "$$$ Restforce (#{ended_at - started_at}s) #{verb} #{args[0]}" if defined?(Rails)
             response
           rescue Restforce::UnauthorizedError
             if retries > 0
